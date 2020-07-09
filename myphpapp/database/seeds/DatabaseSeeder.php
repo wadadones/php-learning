c324bed51c09faad47f9c102bcbd55a8eba6ad8d
wadadones <jones3429@outlook.jp>
2020-07-10 08:37:51 +0900

    hello laravel

---
+++ myphpapp/database/seeds/DatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     * $ php artisan db:seed でこれが実行される
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(BooksTableSeeder::class);
+    }
+}
