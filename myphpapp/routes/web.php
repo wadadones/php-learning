f1c8929bb6c1bc9c2803c2d7d50db84a783dd56e
wadadones <jones3429@outlook.jp>
2020-07-10 09:45:51 +0900

    add calendar in laravel home

--- myphpapp/routes/web.php
+++ myphpapp/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ Route::resource('book', 'BookController');
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', 'HomeController@home');
