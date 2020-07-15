d45522759b76ba9d34f9802be0116a31d883c644
wadadones <jones3429@outlook.jp>
2020-07-15 09:19:08 +0900

    add form

--- myphpapp/resources/views/home.blade.php
+++ myphpapp/resources/views/home.blade.php
@@ -38,19 +38,15 @@
       </tbody>
     </table>
   
-    <form action="/submit">
+    <form action="/submit" method="post">
       <div class="form-group">
         <label for="date-input">Date</label>
-        <input type="email" class="form-control" id="date-input" disabled>
+        <input type="text" class="form-control" id="date-input" disabled>
         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
       </div>
       <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-      </div>
-      <div class="form-group form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        <label for="name-input">Name</label>
+        <input type="text" class="form-control" id="name-input" placeholder="Name">
       </div>
       <button type="submit" class="btn btn-primary">Submit</button>
     </form>
